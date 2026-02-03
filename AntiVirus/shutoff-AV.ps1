1acf6983d53984df8624e239b7749b0d97e414bb
Ncastaldi <Nick.Castaldi@its.ny.gov>
2026-02-03 03:25:58 -0500

    Update shutoff-AV.ps1

--- AntiVirus/shutoff-AV.ps1
+++ AntiVirus/shutoff-AV.ps1
@@ -2,8 +2,8 @@ Set-MpPreference -DisableRealtimeMonitoring $true
 
 
 Set-MpPreference -DisableIntrusionPreventionSystem $true
-Set-MpPreference -DisableIOAVProtection $true (scans all downloaded files and attachments)
+Set-MpPreference -DisableIOAVProtection $true 
 Set-MpPreference -DisableScriptScanning $true
 Set-MpPreference -EnableControlledFolderAccess Disabled
-Set-MpPreference -SubmitSamplesConsent NeverSend (stops automatic sample submission)
-Set-MpPreference -MAPSReporting Disable (stops cloud-based protection reporting) 
+Set-MpPreference -SubmitSamplesConsent NeverSend 
+Set-MpPreference -MAPSReporting Disable 
